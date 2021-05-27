da5b053ff5c1d0c4adc32fa9efa53739a52ffe53
Adit Patel <pateladit2001@gmail.com>
2021-05-27 15:10:58 +0530

    first commit

---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Coin.css'
+
+export default function Coin({name, image, symbol, price, volume, priceChange, marketcap}) {
+    return (
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt={name} />
+                    <h1>{name}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">${price}</p>
+                    <p className="coin-volume">${volume.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                    {priceChange<0?(
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                    ):(
+                        <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+                    )
+                    }
+                    <p className="coin-marketcap">
+                        Mkt Cap: ${marketcap.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
